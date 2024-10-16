23809ec9cebd9a2f0f4ab35e653529370ed76910
María José Santofimia Romero <mariajose.santofimia@uclm.es>
2024-10-16 16:51:22 +0200

    Add files via upload

---
+++ GPIO/sketch.ino
@@ -0,0 +1,38 @@
+#define onSwitch 15
+#define offSwitch 21
+
+static volatile bool g_flash;
+
+
+
+void IRAM_ATTR on_handleInterrupt(){
+  digitalWrite(25, 1);
+  digitalWrite(12, 1);
+}
+
+
+void IRAM_ATTR off_handleInterrupt(){
+  digitalWrite(25, 0);
+  digitalWrite(12, 0);  
+}
+
+
+void setup() {
+  // put your setup code here, to run once:
+  
+  pinMode(25, OUTPUT);
+  pinMode(12, OUTPUT);
+  
+  pinMode(onSwitch, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(onSwitch), &on_handleInterrupt, FALLING);
+
+
+  pinMode(offSwitch, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(offSwitch), &off_handleInterrupt, FALLING); 
+}
+
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
